e13f43e27be7d776b55899670dd431f3540c54fe
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-08-13 20:17:05 +0900

    Load cognitive environment vocabulary dataset

--- dataset-extension-20260811.js
+++ dataset-extension-20260811.js
@@ -5,6 +5,7 @@
     "data/research-20260811-judgment-process.json",
     "data/research-20260812-learning-judgment.json",
     "data/research-20260812-perception-expression.json",
+    "data/research-20260813-cognitive-environment.json",
   ];
 
   window.fetch = async (...args) => {
